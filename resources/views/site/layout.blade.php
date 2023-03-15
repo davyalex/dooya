44f2cadb7e10e7b8f3d04ae3268b66ae11aec4f6
davyalex <alexkouamelan96pv@gmail.com>
2023-03-15 11:11:13 +0000

    optimize deploy

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -252,7 +252,7 @@
               <!-- Mobile Vertical Menu Start Here -->
               <div class="container d-block d-lg-none">
                   <div class="vertical-menu mt-30">
-                      <span class="categorie-title mobile-categorei-menu">Categories</span>
+                      <span class="categorie-title mobile-categorei-menu"> Categories</span>
                       <nav>  
                           <div id="cate-mobile-toggle" class="category-menu sidebar-menu sidbar-style mobile-categorei-menu-list menu-hidden ">
                               <ul>
